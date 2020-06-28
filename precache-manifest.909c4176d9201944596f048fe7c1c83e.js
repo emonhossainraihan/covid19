a9a1dfc84ff6ddd9cc0881bbd6cb83eea8d44d9f
emonhossainraihan <mdroy7475@gmail.com>
2020-06-29 01:03:27 +0600

    Updates

---
+++ precache-manifest.909c4176d9201944596f048fe7c1c83e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263f5254192e5b1ad62489807ce90db7",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "ad145a619b6b3c374376",
+    "url": "/covid19/static/css/main.f834ee3f.chunk.css"
+  },
+  {
+    "revision": "05b20b5cf8223d78de18",
+    "url": "/covid19/static/js/2.7b9dbbf0.chunk.js"
+  },
+  {
+    "revision": "1676cfeecbcf61be862405d69569c73d",
+    "url": "/covid19/static/js/2.7b9dbbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad145a619b6b3c374376",
+    "url": "/covid19/static/js/main.c561d8ff.chunk.js"
+  },
+  {
+    "revision": "34491f16cdd8cac9985a",
+    "url": "/covid19/static/js/runtime-main.fa1deedf.js"
+  },
+  {
+    "revision": "feea5fb1c98ed41237fa5d7d7be3c822",
+    "url": "/covid19/static/media/cases.feea5fb1.png"
+  },
+  {
+    "revision": "e15730a5bffa8dcd129173f1bb3748f5",
+    "url": "/covid19/static/media/corona.e15730a5.svg"
+  },
+  {
+    "revision": "b379f9d1c1c8700841b2439d529f9919",
+    "url": "/covid19/static/media/deaths-large.b379f9d1.png"
+  },
+  {
+    "revision": "66525ad43d232f75d95044619962bb3b",
+    "url": "/covid19/static/media/recovered.66525ad4.png"
+  }
+]);
